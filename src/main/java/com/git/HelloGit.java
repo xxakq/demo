406a788869c82c00defd29e03345c077c64b4ab9
jack <jack@qq.com>
2020-07-27 01:49:03 +0800

    hot_fix

--- src/main/java/com/git/HelloGit.java
+++ src/main/java/com/git/HelloGit.java
@@ -2,11 +2,6 @@ package com.git;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("update one");
-        System.out.println("创建了分支");
-        System.out.println("主干添加");
-        System.out.println("分支提交");
-        System.out.println("github提交");
+        System.out.println("hot_fix");
     }
 }
